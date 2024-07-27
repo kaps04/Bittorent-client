dc4abda68e5ee89fee3c1c26466982fc2cdecb96
kaps04 <kaps.acharya4@gmail.com>
2024-07-27 19:56:51 +0545

    integers decoding

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ import bencodepy
 # - decode_bencode(b"5:hello") -> b"hello"
 # - decode_bencode(b"10:hello12345") -> b"hello12345"
 def decode_bencode(bencoded_value):
+    
     if chr(bencoded_value[0]).isdigit():
         first_colon_index = bencoded_value.find(b":")
         if first_colon_index == -1:
